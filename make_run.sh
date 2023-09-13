e571dad07bc9cc7f4046d61f0965dde40d0472b7
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-09-14 05:15:01 +1000

    reset

--- make_run.sh
+++ make_run.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-make clean build && build/./OpenGLExample
+
+# Set the build mode variable based on the argument (default is 'debug')
+if [ $# -gt 0 ]; then
+  if [ "$1" = "release" ]; then
+    export BUILD_MODE="release"
+  else
+    export BUILD_MODE="debug"
+  fi
+else
+  export BUILD_MODE="debug"
+fi
+
+# Clean and build with the specified mode
+make clean
+make
+if [ $? -eq 0 ]; then
+  echo "Build successful"
+  build/./OpenGLExample
+else
+  echo "Build failed"
+fi
